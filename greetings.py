31829b328fff130f65d9170a0055b43ad3fe3f01
fvoneelix <duetsch.felix@gmx.de>
2025-05-20 16:47:12 +0200

    Geschafft daten zu Importienen als hdl

--- greetings.py
+++ greetings.py
@@ -1,1 +1,26 @@
-# Hallo Felix
+#Hallo Felix
+#Hallo Zaizai
+import earthaccess
+import pprint
+import os
+
+auth = earthaccess.login(strategy="environment")
+
+bounding_box=(-18.0, 12.0, 35.0, 20.0)  # Sahel-Zone Bounding Box: [West, South, East, North]
+
+
+
+# Zeitspanne
+temporal = ("2022-01-01", "2022-12-31")
+
+# Suche z.B. nach MODIS Vegetation Index
+results = earthaccess.search_data(
+    short_name="MOD13A2",  # MODIS NDVI 16-Day product
+    bounding_box=bounding_box,
+    temporal=temporal,
+    cloud_hosted=True
+)
+
+
+earthaccess.download(results[:3], local_path="modis_sahel")  # lädt in diesen Ordner
+
